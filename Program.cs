894eec499b1e9caa6d181f2f99e9e35343dce95e
shaikaakheel <63797162+shaikaakheel@users.noreply.github.com>
2023-01-02 09:29:01 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using ScriptCompile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aakheel a = new Aakheel(@"C:\Users\AShaik\Package.dtsx");
+            Console.ReadLine();
+        }
+    }
+}
